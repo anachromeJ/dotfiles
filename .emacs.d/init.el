130bf7ea5bcf54c17c08fa4019b935150769e9d8
Jincheng Li <jincheng_li@brown.edu>
2013-10-28 17:18:56 -0400

    added markdown mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -22,6 +22,7 @@
 (show-paren-mode t)
 (global-linum-mode t)
 (column-number-mode t)
+(setq-default cursor-type 'bar)
 (put 'downcase-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
 (setq column-number-mode t)
@@ -36,24 +37,28 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit autoface-default :strike-through nil :underline nil :slant normal :weight normal :height 120 :width normal :family "monaco"))))
- '(column-marker-1 ((t (:background "red"))))
- '(diff-added ((t (:foreground "cyan"))))
- '(flymake-errline ((((class color) (background light)) (:background "Red"))))
- '(font-lock-comment-face ((((class color) (min-colors 8) (background light)) (:foreground "red"))))
- '(fundamental-mode-default ((t (:inherit default))))
- '(highlight ((((class color) (min-colors 8)) (:background "white" :foreground "magenta"))))
- '(isearch ((((class color) (min-colors 8)) (:background "yellow" :foreground "black"))))
- '(linum ((t (:foreground "black" :weight bold))))
- '(region ((((class color) (min-colors 8)) (:background "white" :foreground "magenta"))))
- '(secondary-selection ((((class color) (min-colors 8)) (:background "gray" :foreground "cyan"))))
- '(show-paren-match ((((class color) (background light)) (:background "black"))))
- '(vertical-border ((t nil)))
+;;  '(default ((t (:inherit autoface-default :strike-through nil :underline nil :slant normal :weight normal :height 120 :width normal :family "monaco"))))
+;;  '(column-marker-1 ((t (:background "red"))))
+;;  '(diff-added ((t (:foreground "cyan"))))
+;;  '(flymake-errline ((((class color) (background light)) (:background "Red"))))
+;;  '(font-lock-comment-face ((((class color) (min-colors 8) (background light)) (:foreground "red"))))
+;;  '(fundamental-mode-default ((t (:inherit default))))
+;;  '(highlight ((((class color) (min-colors 8)) (:background "white" :foreground "magenta"))))
+;;  '(isearch ((((class color) (min-colors 8)) (:background "yellow" :foreground "black"))))
+;;  '(linum ((t (:foreground "black" :weight bold))))
+;;  '(region ((((class color) (min-colors 8)) (:background "white" :foreground "magenta"))))
+;;  '(secondary-selection ((((class color) (min-colors 8)) (:background "gray" :foreground "cyan"))))
+;;  '(show-paren-match ((((class color) (background light)) (:background "black"))))
+;;  '(vertical-border ((t nil)))
 )
 
 ;; ------------
 ;; -- Macros --
 ;; ------------
+
+(global-unset-key "\C-z")
+(global-unset-key "\C-x\C-c")
+
 (load "defuns-config.el")
 (fset 'align-equals "\C-[xalign-regex\C-m=\C-m")
 (global-set-key "\M-=" 'align-equals)
@@ -69,12 +74,30 @@
 (global-set-key "\M-h" 'backward-delete-word)
 (global-set-key "\M-u" 'zap-to-char)
 
+;; bind control-x-r to revert buffer without confirmation
+(defun revert-buffer-no-confirm ()
+  (interactive) (revert-buffer t t))
+(global-set-key "\C-x\C-r" 'revert-buffer-no-confirm)
+
+
 ;; ---------------------------
 ;; -- JS Mode configuration --
 ;; ---------------------------
 (load "js-config.el")
 (add-to-list 'load-path "~/.emacs.d/jade-mode") ;; github.com/brianc/jade-mode
 (require 'sws-mode)
-(require 'jade-mode)    
+(require 'jade-mode)
 (add-to-list 'auto-mode-alist '("\\.styl$" . sws-mode))
 (add-to-list 'auto-mode-alist '("\\.jade$" . jade-mode))
+
+;; -------------------
+;; -- Markdown Mode --
+;; -------------------
+(add-to-list 'load-path "~/.emacs.d/markdown-mode") ;; http://jblevins.org/git/markdown-mode.git
+(autoload 'markdown-mode "markdown-mode"
+   "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+
