cf1a62de58864d5ccdea41075c4cffdde34bbb01
Jincheng Li <jincheng_li@brown.edu>
2015-02-07 22:41:46 -0500

    xcscope, powerline and better theme

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -35,6 +35,7 @@
 (setq initial-scratch-message "")
 (setq-default tab-width 4)
 (setq-default c-basic-offset 4)
+(setq-default indent-tabs-mode nil)
 ;; (setq visible-bell t)
 (scroll-bar-mode 0)
 (tool-bar-mode 0)
@@ -42,6 +43,9 @@
 ;; ------------------
 ;; -- Color Themes --
 ;; ------------------
+;; (require 'moe-theme)
+;; (moe-theme-set-color 'orange)
+; (powerline-moe-theme)
 ;; (load-theme 'solarized-light)
 ;; (require 'color-theme)
 ;; (color-theme-initialize)
@@ -98,6 +102,10 @@
     (auto-complete-mode 1)))
 (global-auto-complete-mode t)
 
+;; -- Cscope --
+(require 'xcscope)
+(cscope-setup)
+
 ;; -------------
 ;; -- JS Mode --
 ;; -------------
@@ -131,14 +139,23 @@
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ansi-color-faces-vector [default bold shadow italic underline bold bold-italic bold])
- '(custom-enabled-themes (quote (solarized-light)))
- '(custom-safe-themes (quote ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default))))
+ '(ansi-color-names-vector ["#fdf6e3" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#657b83"])
+ '(custom-enabled-themes (quote (tango)))
+ '(custom-safe-themes (quote ("dd4db38519d2ad7eb9e2f30bc03fba61a7af49a185edfd44e020aa5345e3dca7" "14fc90b23d8eb37a16911992216e4ecf51209adb664be1b43d7ea87416a5dc45" "d50ab16e07f2a4b2bb16e12cd27d4c6a7a79396631c50f6fc46c0d5899acd81d" "74278d14b7d5cf691c4d846a4bbf6e62d32104986f104c1e61f718f9669ec04b" "bc2846e7b3fe884da451356d94e37e56441e4b19ea058b73faa8a055f55a028b" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default)))
+ '(fci-rule-color "#eee8d5")
+ '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
+ '(highlight-tail-colors (quote (("#eee8d5" . 0) ("#B4C342" . 20) ("#69CABF" . 30) ("#69B7F0" . 50) ("#DEB542" . 60) ("#F2804F" . 70) ("#F771AC" . 85) ("#eee8d5" . 100))))
+ '(vc-annotate-background "#586e75")
+ '(vc-annotate-color-map (quote ((20 . "#990A1B") (40 . "#FF6E64") (60 . "#cb4b16") (80 . "#7B6000") (100 . "#b58900") (120 . "#DEB542") (140 . "#546E00") (160 . "#859900") (180 . "#B4C342") (200 . "#3F4D91") (220 . "#6c71c4") (240 . "#9EA0E5") (260 . "#2aa198") (280 . "#69CABF") (300 . "#00629D") (320 . "#268bd2") (340 . "#69B7F0") (360 . "#d33682"))))
+ '(vc-annotate-very-old-color "#93115C"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
